1a94d4abe065308c02906315e3542263e2eabac6
Filip Urban <info@filipurban.cz>
2021-09-13 16:11:13 +0200

    optimize

--- app/RouterFactory.php
+++ app/RouterFactory.php
@@ -17,9 +17,9 @@ class RouterFactory
 		$router->withModule('Admin')
 			->addRoute('admin/<presenter>/<action>[/<id>]', 'Homepage:default');
 
+		$router->withModule('Front')->addRoute('[<lang=cs (cs)>/]', 'Homepage:default');
 		$router->withModule('Front')->addRoute('[<lang=cs (cs)>/]<slug>', 'Homepage:service');
-		$router->withModule('Front')
-			->addRoute('[<lang=cs (cs)>/]<presenter>/<action>', 'Homepage:default');
+		$router->withModule('Front')->addRoute('[<lang=cs [a-z]{2}>/]<presenter>/<action>', 'Error:404');
 
 
 
